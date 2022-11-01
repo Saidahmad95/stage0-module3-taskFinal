db5dad20a76f4099257973c4936900b0f66efe3d
Saidahmad95 <murtazoyev00@gmail.com>
2022-11-02 02:37:19 +0500

    first commit

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -1,6 +1,7 @@
 package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
-    public void calculateSum(int number) {
+    public void calculateSum(int number) {// 8369
+        System.out.println(number%10+(number%100)/10+(number/100)%10+number/1000);
     }
 }
